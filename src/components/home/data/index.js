1dec3bfcc1e0094d19f91ba3c5add2c24427296c
AgusKz <agustin.fernandez@kzsoftworks.com>
2023-03-23 12:37:01 -0300

    profile pic updated

--- src/components/home/data/index.js
+++ src/components/home/data/index.js
@@ -1,20 +1,23 @@
 import React from "react";
-import {ReactComponent as Hello} from '../../../assets/send.svg'
+import { ReactComponent as Hello } from "../../../assets/send.svg";
 
-import '../'
+import "../";
 
 const Data = () => {
   return (
     <div className="home__data">
-      <h1 className="home__title">Agustin F 👋</h1>
+      <h1 className="home__title">Agus 👋</h1>
       <h3 className="home__subtitle">Software Engineer 🇺🇾</h3>
-      <p className="home__description">Hey, I'm Agus! Love learning new things and living new experiences. Lets get in touch!</p>
+      <p className="home__description">
+        Hey, I'm Agustin! Love learning new things and living new experiences.
+        Lets get in touch!
+      </p>
       <a href="#contact" className="button button--flex">
         Say Hello
         <Hello />
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default Data
+export default Data;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import './styles.css'
-import Social from './social'
-import Data from './data'
-import ScrollDown from './scrolldown'
+import React from "react";
+import "./styles.css";
+import Social from "./social";
+import Data from "./data";
+import ScrollDown from "./scrolldown";
 
 const Home = () => {
   return (
-    <section className='home section' id="home">
-      <div className='home__container container grid'>
-        <div className='home__content grid'>
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
           <Social />
-          <div className='home__img'></div>
+          <div className="home__img"></div>
           <Data />
         </div>
         <ScrollDown />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
